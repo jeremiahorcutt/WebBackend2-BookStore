12330b329aa1f25188282cd00f4155208137db43
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-10-02 14:20:34 -0600

    updated files

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 const path = require('path');
-const bodyParser = require('body-parser');
+
 const express = require('express');
-const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const User = require('./models/user');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+ app.use((req, res, next) => {
+  User.findById('615483777c00faf81d26e00f')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+}); 
+
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);  
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+mongoose.connect('mongodb+srv://GenericReadWrite:mongoman1@cluster0.obb5n.mongodb.net/shop?retryWrites=true&w=majority').then(result => {
+  User.findOne().then(user => {
+    if(!user) {
+      const user = new User({
+        name: 'Jeremiah',
+        email: 'jeremiah@test.com',
+        cart: {
+          items: []
+        }
+      });
+      user.save();
+    }
+  })
+  app.listen(3000);
+}).catch(err => {
+  console.log(err);
+});
