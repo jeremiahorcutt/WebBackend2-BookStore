da46dce609a2c69a6c25e56792b45e5986e06467
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-10-15 11:53:21 -0600

    updatedfiles from node repo

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
+app.use((error, req, res, next) => {
+  res.status(500).render('500', { 
+    pageTitle: 'Error!', 
+  path: '/500',
+  isAuthenticated: req.session.isLoggedIn });
+})
+
 mongoose
   .connect(
     uri
