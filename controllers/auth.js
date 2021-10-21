de894bce41e74560bc3c9b6082983ad952edecb8
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-10-21 10:52:08 -0600

    trying to fix password controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -222,26 +222,23 @@ exports.postReset = (req, res, next) => {
 
 exports.getNewPassword = (req, res, next) => {
   const token = req.params.token;
-  User.findOne({resetToken: token, resetTokenExpiration: {$gt: Date.now()}})
-  .then(user => {
-    let message = req.flash('error');
-  if (message.length > 0){
-   message = message[0];
-  } else {
-    message = null;
-  }
-  res.render('auth/new-password', {
-    path: '/new-password',
-    pageTitle: 'New Password',
-    errorMessage: message,
-    userId: user_id.toString(),
-    passwordToken: token
-  });
-  })
-  .catch(err => {
-    const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then(user => {
+      let message = req.flash('error');
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'New Password',
+        errorMessage: message,
+        userId: user._id.toString()
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
 };
 
